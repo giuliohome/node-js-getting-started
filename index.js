883791b38962b836ad6f70c233561a5661b04960
giuliohome <giuliohome@gmail.com>
2021-04-17 19:53:32 +0200

    Angular tour of heroes

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 const cool = require('cool-ascii-faces');
 const { Pool } = require('pg');
-const pool = new Pool({
+/* const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
   }
-});
+}); */
+// https://stackoverflow.com/a/62413056
+// if on heroku
+if (process.env.DATABASE_URL) {
+  pool = new Pool({
+      connectionString: process.env.DATABASE_URL,
+      ssl: true,
+  });
+} else {
+// if on local
+  console.log("local db: user",process.env.D_user)
+  pool = new Pool({
+      user: process.env.D_user,
+      password: process.env.D_password,
+      port: process.env.D_pport,
+      host: process.env.D_host,
+      database: process.env.D_database
+  });
+}
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
@@ -27,6 +45,9 @@ express()
       console.error(err);
       res.send("Error " + err);
     }
+  })
+  .get('/toh', (req,res) => {
+    res.sendFile(process.cwd()+"/public/toh/index.html")
   })
   .get('/times', (req, res) => res.send(showTimes()))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
